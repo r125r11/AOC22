007911c00049cebf0affcac5935c30b204037a69
Koert Hup <k.hup125@gmail.com>
2022-12-04 10:02:53 +0100

    Day 4

---
+++ Day4/part1.ts
@@ -0,0 +1,23 @@
+import { input, sample } from "./input";
+
+const output = input
+    .split(/\r?\n/)
+    .map(pair => pair.split(','))
+    .reduce((acc, curr)=>{
+        const left = curr[0].split('-').map(v => Number(v))
+        const right = curr[1].split('-').map(v => Number(v))
+        if(left[0] <= right[0] && left[1] >= right[1]){
+            // acc.push(curr)
+            // console.log(curr)
+            return acc+1
+        }
+        if(left[0] >= right[0] && left[1] <= right[1]){
+            // acc.push(curr)
+            // console.log(curr)
+            return acc+1
+        }
+        return acc
+    },0)
+    //[] as string[][])
+
+console.log(output)
