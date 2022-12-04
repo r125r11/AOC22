c2abcaba452e3b890dea48b7b8824a60257154fb
Koert Hup <k.hup125@gmail.com>
2022-12-04 21:04:16 +0100

    no more comments

--- Day4/part1.ts
+++ Day4/part1.ts
@@ -6,18 +6,10 @@ const output = input
     .reduce((acc, curr)=>{
         const left = curr[0].split('-').map(v => Number(v))
         const right = curr[1].split('-').map(v => Number(v))
-        if(left[0] <= right[0] && left[1] >= right[1]){
-            // acc.push(curr)
-            // console.log(curr)
-            return acc+1
-        }
-        if(left[0] >= right[0] && left[1] <= right[1]){
-            // acc.push(curr)
-            // console.log(curr)
+        if((left[0] <= right[0] && left[1] >= right[1]) || (left[0] >= right[0] && left[1] <= right[1])){
             return acc+1
         }
         return acc
     },0)
-    //[] as string[][])
 
 console.log(output)
--- Day4/part2.ts
+++ Day4/part2.ts
@@ -6,25 +6,10 @@ const output = input
     .reduce((acc, curr)=>{
         const left = curr[0].split('-').map(v => Number(v))
         const right = curr[1].split('-').map(v => Number(v))
-        // a < b && c > d
-        // a < b && c < d && c > b
-        // a > b && c < d
-        // a > b && c > d && a < d
-        if(
-                (left[0] <= right[1] && left[1] >= right[0] )
-            ||  (left[0] >= right[1] && left[1] <= right[1] )
-            ){
-            // acc.push(curr)
-            console.log(curr)
+        if(left[0] <= right[1] && left[1] >= right[0]){
             return acc+1
         }
-        // if(left[0] >= right[0] && (left[1] <= right[1] || left[1] <= right[0])){
-        //     // acc.push(curr)
-        //     // console.log(curr)
-        //     return acc+1
-        // }
         return acc
     },0)
-    //[] as string[][])
 
 console.log(output)
